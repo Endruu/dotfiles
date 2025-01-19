b1aae24ce45c1fe7912b8cfc1e6c57cf69a27ed2
endruu <bibok.andor@gmail.com>
2025-01-19 17:03:38 +0100

    Update git config

--- setup_git.sh
+++ setup_git.sh
@@ -1,7 +1,16 @@
 git lfs install
 
-git config --global alias.co switch
+git config --global alias.s "switch"
 git config --global alias.pr "pull --rebase --autostash"
-git config --global alias.hist "log --graph --oneline"
+
+git config --global alias.l "log --oneline"
+git config --global alias.ll "log --graph --oneline"
+git config --global alias.lll "log --all --decorate --oneline --graph"
+
+git config --global alias.cm "merge --continue"
+git config --global alias.cr "rebase --continue"
+git config --global alias.am "merge --abort"
+git config --global alias.ar "rebase --abort"
+
 git config --global core.editor "nvim --clean"
 git config --global rerere.enabled true
