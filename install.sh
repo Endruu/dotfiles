2b52bd2c8a97ecef7283751101944e1af02900b1
endruu <bibok.andor@gmail.com>
2025-01-19 17:06:48 +0100

    Install nvm, npm, deno, node + more

--- install.sh
+++ install.sh
@@ -10,19 +10,33 @@ sudo apt upgrade -yqq
 sudo apt install -yqq \
 	sudo \
 	curl wget \
+	man \
 	build-essential \
-	gpg ca-certificates software-properties-common \
+	gpg ca-certificates software-properties-common libssl-dev \
 	zsh \
 	tmux \
 	taskwarrior \
-	zip unzip \
+	zip unzip pigz \
 	python3 python3-distutils \
 	ripgrep fd-find fzf \
 	clang-16 clangd-16 clang-format-16 \
+	luarocks \
 	git git-lfs
 
 tmp_dir=`mktemp -d`
 pushd $tmp_dir
+
+# --- Install nvm, npm, node, deno
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install node
+
+curl -fsSL https://deno.land/install.sh | sh
 
 # --- Install rust
 
@@ -32,8 +46,8 @@ source $HOME/.cargo/env
 
 # --- Install go
 
-wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.23.2.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.23.4.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.23.4.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 export GOPATH=$HOME/.go
 
@@ -47,12 +61,12 @@ cargo install bottom
 go install github.com/jesseduffield/lazygit@latest
 go install github.com/gokcehan/lf@latest
 
-curl -LsSf https://astral.sh/uv/0.5.4/install.sh | sh
+curl -LsSf https://astral.sh/uv/0.5.14/install.sh | sh
 source $HOME/.local/bin/env
 
 # --- Install nvim
 
-wget https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-linux64.tar.gz
+wget https://github.com/neovim/neovim/releases/download/v0.10.3/nvim-linux64.tar.gz
 tar xzf nvim-linux64.tar.gz
 sudo cp -r nvim-linux64/* /usr/
 
