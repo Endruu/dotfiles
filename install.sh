bd75088e33658667c50c58d08bc3192e7131d1e5
endruu <bibok.andor@gmail.com>
2025-01-19 18:31:01 +0100

    Bump go

--- install.sh
+++ install.sh
@@ -46,8 +46,8 @@ source $HOME/.cargo/env
 
 # --- Install go
 
-wget https://go.dev/dl/go1.23.4.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.23.4.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.23.5.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.23.5.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 export GOPATH=$HOME/.go
 
