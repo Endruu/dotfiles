2338c8c908c806fc1efd16690fef5d0012e0770e
endruu <bibok.andor@gmail.com>
2025-03-22 16:33:37 +0100

    Use gitsigns to navigate between hunks

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,1 +1,16 @@
-return { 'lewis6991/gitsigns.nvim', opts = {} }
+return {
+  'lewis6991/gitsigns.nvim',
+  event = 'BufEnter',
+  opts = {
+    on_attach = function(bufnr)
+      local gitsigns = require('gitsigns')
+
+      local map_n = function(keys, func, desc)
+        require("config.helpers").map_n(keys, func, desc, { buffer = bufnr })
+      end
+
+      map_n(']h', function() gitsigns.nav_hunk('next') end, "Go to next hunk")
+      map_n('[h', function() gitsigns.nav_hunk('prev') end, "Go to prev hunk")
+    end,
+  },
+}
