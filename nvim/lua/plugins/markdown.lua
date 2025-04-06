04b915bd8663498556e6dd63d966edba422c66b1
endruu <bibok.andor@gmail.com>
2025-04-06 16:25:39 +0200

    Add markdown rendering

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,10 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {},
+  keys = {
+    { '<leader>tmd', ':RenderMarkdown toggle<CR>', desc = 'Toggle RenderMarkdown' },
+  },
+}
